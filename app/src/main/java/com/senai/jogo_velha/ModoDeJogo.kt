280d1455949ddc4440e9a5740154ed0fdabefce7
LEILA ROSA PEREIRA <21276364@CFP127ALUNO.LOCAL>
2022-03-15 16:53:01 -0300

    primeiras telas feitas

---
+++ app/src/main/java/com/senai/jogo_velha/ModoDeJogo.kt
@@ -0,0 +1,11 @@
+package com.senai.jogo_velha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModoDeJogo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modo_de_jogo)
+    }
+}
