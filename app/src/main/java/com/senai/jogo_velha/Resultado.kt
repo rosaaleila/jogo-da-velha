6e45d2d702ef1725bdebdc48eda90917dfb0fac4
LEILA ROSA PEREIRA <21276364@CFP127ALUNO.LOCAL>
2022-04-12 16:50:59 -0300

    player vs player funcionando

--- app/src/main/java/com/senai/jogo_velha/Resultado.kt
+++ app/src/main/java/com/senai/jogo_velha/Resultado.kt
@@ -1,11 +1,39 @@
 package com.senai.jogo_velha
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Resultado : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        val resultado = findViewById<TextView>(R.id.resultado)
+        val novoJogo = findViewById<Button>(R.id.jogarNovamente)
+        val sair = findViewById<Button>(R.id.sair)
+
+        val intent: Intent = getIntent()
+
+        val empate = intent.getStringExtra("empate")
+
+        if(empate.equals("false")) {
+            val ganhador = intent.getStringExtra("ganhador")
+            resultado.text = "O vencedor foi ${ganhador}!"
+        } else {
+            resultado.text = "Houve um empate!"
+        }
+
+        novoJogo.setOnClickListener {
+            val intent = Intent(this, ModoDeJogo::class.java)
+            startActivity(intent)
+        }
+
+        sair.setOnClickListener {
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/senai/jogo_velha/jogoPlayer.kt
+++ app/src/main/java/com/senai/jogo_velha/jogoPlayer.kt
@@ -4,8 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.GridLayout
 import android.widget.TextView
-import android.widget.Toast
 
 private lateinit var botao1: Button
 private lateinit var botao2: Button
@@ -17,11 +17,11 @@ private lateinit var botao7: Button
 private lateinit var botao8: Button
 private lateinit var botao9: Button
 private lateinit var turnoTextView: TextView
+private lateinit var containerLayouts: GridLayout
 
-var jogadorAtual = "Jogador 1"
-var fimDeJogo = false
-var posicoesJogador1 = ArrayList<Int>()
-var posicoesJogador2 = ArrayList<Int>()
+var jogadorAtual: String = "Jogador 1"
+var posicoesJogador1: MutableList<Button> = mutableListOf()
+var posicoesJogador2: MutableList<Button> = mutableListOf()
 
 class jogoPlayer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,157 +37,155 @@ class jogoPlayer : AppCompatActivity() {
         botao7 = findViewById(R.id.botao7)
         botao8 = findViewById(R.id.botao8)
         botao9 = findViewById(R.id.botao9)
+        containerLayouts = findViewById(R.id.container_botoes)
+
+        turnoTextView = findViewById(R.id.turno)
+
+        calcularJogadas()
+
+    }
+
+    private fun calcularJogadas() {
+
+        botao1.setOnClickListener {
+            if (jogadorAtual == "Jogador 1") {
+                jogadaPlayer1(botao1)
+                posicoesJogador1.add(botao1)
+            } else if (jogadorAtual == "Jogador 2") {
+                jogadaPlayer2(botao1)
+                posicoesJogador2.add(botao1)
+            }
+        }
+
+        botao2.setOnClickListener {
+            if (jogadorAtual == "Jogador 1") {
+                jogadaPlayer1(botao2)
+                posicoesJogador1.add(botao2)
+            } else if (jogadorAtual == "Jogador 2") {
+                jogadaPlayer2(botao2)
+                posicoesJogador2.add(botao2)
+            }
+        }
+
+        botao3.setOnClickListener {
+            if (jogadorAtual == "Jogador 1") {
+                jogadaPlayer1(botao3)
+
+            } else if (jogadorAtual == "Jogador 2") {
+                jogadaPlayer2(botao3)
+            }
+
+        }
+
+        botao4.setOnClickListener {
+            if (jogadorAtual == "Jogador 1") {
+                jogadaPlayer1(botao4)
+
+            } else if (jogadorAtual == "Jogador 2") {
+                jogadaPlayer2(botao4)
+
+            }
+
+        }
+
+        botao5.setOnClickListener {
+            if (jogadorAtual == "Jogador 1") {
+                jogadaPlayer1(botao5)
+
+            } else if (jogadorAtual == "Jogador 2") {
+                jogadaPlayer2(botao5)
+
+            }
+
+        }
+
+        botao6.setOnClickListener {
+            if (jogadorAtual == "Jogador 1") {
+                jogadaPlayer1(botao6)
+            } else if (jogadorAtual == "Jogador 2") {
+                jogadaPlayer2(botao6)
+            }
+        }
+
+        botao7.setOnClickListener {
+            if (jogadorAtual == "Jogador 1") {
+                jogadaPlayer1(botao7)
+
+            } else if (jogadorAtual == "Jogador 2") {
+                jogadaPlayer2(botao7)
+
+            }
+
+        }
+
+        botao8.setOnClickListener {
+            if (jogadorAtual == "Jogador 1") {
+                jogadaPlayer1(botao8)
+
+            } else if (jogadorAtual == "Jogador 2") {
+                jogadaPlayer2(botao8)
+
+            }
+        }
+
+        botao9.setOnClickListener {
+            if (jogadorAtual == "Jogador 1") {
+                jogadaPlayer1(botao9)
+            } else if (jogadorAtual == "Jogador 2") {
+                jogadaPlayer2(botao9)
+            }
+
+        }
+
+    }
+
+    private fun jogadaPlayer2(botao: Button) {
+        botao.text = "O"
+        jogadorAtual = "Jogador 1"
+        botao.isEnabled = false
+        verificarGanhador("O")
+    }
+
+    private fun jogadaPlayer1(botao: Button) {
+        botao.text = "X"
+        jogadorAtual = "Jogador 2"
+        botao.isEnabled = false
+        verificarGanhador("X")
+    }
+
+    private fun verificarGanhador(jogador: String) {
 
         val intent = Intent(this, Resultado::class.java)
+        intent.putExtra("ganhador", jogador)
 
-        turnoTextView = findViewById<TextView>(R.id.turno)
-
-        botao1.setOnClickListener() {
-            if (jogadorAtual == "Jogador 1") {
-                botao1.text = "X"
-                jogadorAtual = "Jogador 2"
-                botao1.isEnabled = false
-                posicoesJogador1.add(1)
-
-            } else {
-                botao1.text = "O"
-                jogadorAtual = "Jogador 1"
-                botao1.isEnabled = false
-                posicoesJogador2.add(1)
-            }
-        }
-
-        botao2.setOnClickListener() {
-            if (jogadorAtual == "Jogador 1") {
-                botao2.text = "X"
-                jogadorAtual = "Jogador 2"
-                botao2.isEnabled = false
-                posicoesJogador1.add(2)
-
-            } else {
-                botao2.text = "O"
-                jogadorAtual = "Jogador 1"
-                botao2.isEnabled = false
-                posicoesJogador2.add(2)
-
-            }
-        }
-
-        botao3.setOnClickListener() {
-            if (jogadorAtual.equals("Jogador 1")) {
-                botao3.text = "X"
-                jogadorAtual = "Jogador 2"
-                botao3.isEnabled = false
-                posicoesJogador1.add(3)
-
-            } else {
-                botao3.text = "O"
-                jogadorAtual = "Jogador 1"
-                botao3.isEnabled = false
-                posicoesJogador2.add(3)
-
-            }
-        }
-
-        botao4.setOnClickListener() {
-            if (jogadorAtual.equals("Jogador 1")) {
-                botao4.text = "X"
-                jogadorAtual = "Jogador 2"
-                botao4.isEnabled = false
-                posicoesJogador1.add(4)
-
-            } else {
-                botao4.text = "O"
-                jogadorAtual = "Jogador 1"
-                botao4.isEnabled = false
-                posicoesJogador2.add(4)
-
-            }
-        }
-
-        botao5.setOnClickListener() {
-            if (jogadorAtual.equals("Jogador 1")) {
-                botao5.text = "X"
-                jogadorAtual = "Jogador 2"
-                botao5.isEnabled = false
-                posicoesJogador1.add(5)
-
-            } else {
-                botao5.text = "O"
-                jogadorAtual = "Jogador 1"
-                botao5.isEnabled = false
-                posicoesJogador2.add(5)
-
-            }
-        }
-
-        botao6.setOnClickListener() {
-            if (jogadorAtual.equals("Jogador 1")) {
-                botao6.text = "X"
-                jogadorAtual = "Jogador 2"
-                botao6.isEnabled = false
-                posicoesJogador1.add(6)
-
-            } else {
-                botao6.text = "O"
-                jogadorAtual = "Jogador 1"
-                botao6.isEnabled = false
-                posicoesJogador2.add(6)
-
-            }
-        }
-
-        botao7.setOnClickListener() {
-            if (jogadorAtual.equals("Jogador 1")) {
-                botao7.text = "X"
-                jogadorAtual = "Jogador 2"
-                botao7.isEnabled = false
-                posicoesJogador1.add(7)
-
-            } else {
-                botao7.text = "O"
-                jogadorAtual = "Jogador 1"
-                botao7.isEnabled = false
-                posicoesJogador2.add(7)
-
-            }
-        }
-
-        botao8.setOnClickListener() {
-            if (jogadorAtual.equals("Jogador 1")) {
-                botao8.text = "X"
-                jogadorAtual = "Jogador 2"
-                botao8.isEnabled = false
-                posicoesJogador1.add(8)
-
-            } else {
-                botao8.text = "O"
-                jogadorAtual = "Jogador 1"
-                botao8.isEnabled = false
-                posicoesJogador2.add(8)
-
-            }
-        }
-
-        botao9.setOnClickListener() {
-            if (jogadorAtual.equals("Jogador 1")) {
-                botao9.text = "X"
-                jogadorAtual = "Jogador 2"
-                botao9.isEnabled = false
-                posicoesJogador1.add(9)
-            } else {
-                botao9.text = "O"
-                jogadorAtual = "Jogador 1"
-                botao9.isEnabled = false
-                posicoesJogador2.add(9)
-            }
-        }
-
-        if (posicoesJogador1.size >= 3) {
+        if (botao1.text == jogador && botao2.text == jogador && botao3.text == jogador) {
+            intent.putExtra("empate", "false")
+            startActivity(intent)
+        } else if (botao4.text == jogador && botao5.text == jogador && botao6.text == jogador) {
+            intent.putExtra("empate", "false")
+            startActivity(intent)
+        } else if (botao7.text == jogador && botao8.text == jogador && botao9.text == jogador) {
+            intent.putExtra("empate", "false")
+            startActivity(intent)
+        } else if (botao1.text == jogador && botao5.text == jogador && botao9.text == jogador) {
+            intent.putExtra("empate", "false")
+            startActivity(intent)
+        } else if (botao3.text == jogador && botao5.text == jogador && botao7.text == jogador) {
+            intent.putExtra("empate", "false")
+            startActivity(intent)
+        } else if (botao1.text == jogador && botao4.text == jogador && botao7.text == jogador) {
+            intent.putExtra("empate", "false")
+            startActivity(intent)
+        } else if (botao2.text == jogador && botao5.text == jogador && botao8.text == jogador) {
+            intent.putExtra("empate", "false")
+            startActivity(intent)
+        } else if (botao3.text == jogador && botao6.text == jogador && botao9.text == jogador) {
+            intent.putExtra("empate", "false")
+            startActivity(intent)
+        } else if (!botao1.isEnabled && !botao2.isEnabled && !botao3.isEnabled && !botao4.isEnabled && !botao5.isEnabled && !botao6.isEnabled && !botao7.isEnabled && !botao8.isEnabled && !botao9.isEnabled) {
+            intent.putExtra("empate", "true")
             startActivity(intent)
         }
-
-
     }
 
 }
